9c3bad7d060c71d045d05a16b8a9a9e7fdd74fe0
Nemanja Babic <Nemanja.Babic@Hypertherm.com>
2021-04-05 09:23:31 -0400

    Implemented Json Data Template Selector

---
+++ ItemTemplateSelector/JsonDataItemTemplateSelector.cs
@@ -0,0 +1,44 @@
+﻿namespace JsonEditorSharp.ItemTemplateSelector
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using Newtonsoft.Json.Linq;
+
+    public class JsonDataItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate JsonPrimitivePropertyTemplate { get; set; }
+
+        public DataTemplate JsonArrayPropertyTemplate { get; set; }
+
+        public DataTemplate JsonObjectPropertyTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item == null)
+            {
+                return null;
+            }
+
+            if (container is not FrameworkElement frameworkElement)
+            {
+                return null;
+            }
+
+            Type type = item.GetType();
+            if (type == typeof(JProperty))
+            {
+                if (item is JProperty jProperty)
+                    return jProperty.Value.Type switch
+                    {
+                        JTokenType.Object => frameworkElement.FindResource("JsonObjectPropertyTemplate") as DataTemplate,
+                        JTokenType.Array => frameworkElement.FindResource("JsonArrayPropertyTemplate") as DataTemplate,
+                        _ => frameworkElement.FindResource("JsonPrimitivePropertyTemplate") as DataTemplate
+                    };
+            }
+
+            var key = new DataTemplateKey(type);
+            return frameworkElement.FindResource(key) as DataTemplate;
+        }
+    }
+}
